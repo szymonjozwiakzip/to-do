7d871c75acf5c29f57a7035c50e5dc5f820501b1
szymonjozwiakzip <jozwiakszymon03@gmail.com>
2024-04-11 19:53:00 +0200

    style and some functionality changed

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById('addButton').addEventListener('click', function(){
-    const newTask = document.getElementById('taskInput').value.trim();
-    if (newTask !== '') {
-      document.getElementById('message').textContent ='';
-      const newTaskLi = document.createElement('li');
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("addButton").addEventListener("click", function () {
+    const newTask = document.getElementById("taskInput").value.trim();
+    if (newTask !== "") {
+      document.getElementById("message").textContent = "";
+      const newTaskLi = document.createElement("li");
       newTaskLi.textContent = newTask;
-      document.getElementById('taskList').appendChild(newTaskLi);
-      document.getElementById('taskInput').value='';
+      document.getElementById("taskList").appendChild(newTaskLi);
+      document.getElementById("taskInput").value = "";
     } else {
-      document.getElementById('message').textContent='Please fill out the field';
+      document.getElementById("message").textContent =
+        "Please fill out the field";
     }
   });
 
-  document.getElementById('taskList').addEventListener('click', function(e){
+  document.getElementById("taskList").addEventListener("click", function (e) {
     const taskItem = e.target;
     this.removeChild(taskItem);
   });
 
-  document.getElementById('printButton').addEventListener('click', function(){
-    const printContent = document.getElementById('printArea').innerHTML;
-    const printContentWithHeader = '<h2>MY TASK LIST</h2>' + printContent;
+  document.getElementById("printButton").addEventListener("click", function () {
+    const printContent = document.getElementById("printArea").innerHTML;
+    const printContentWithHeader = "<h2>MY TASK LIST</h2>" + printContent;
     document.body.innerHTML = printContentWithHeader;
     window.print();
     window.location.reload();
